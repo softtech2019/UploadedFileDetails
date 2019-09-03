f9c17b06715923a9d15ae595288035e92f97ec7f
rudph1 <rudph1@ac.uk>
2019-09-04 00:31:58 +0100

    Signup Tech Added

---
+++ UPDowload/Controllers/EmailController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Net;
+using System.Net.Mail;
+using System.Web.Mvc;
+
+namespace UPDowload.Controllers
+{
+    public class EmailController : Controller
+    {
+        // GET: Email
+        public ActionResult SendEmail()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult SendEmail(string receiver, string subject, string message)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    var senderEmail = new MailAddress("sageer.shahzad@gmail.com", "Sageer");
+                    var receiverEmail = new MailAddress(receiver, "Receiver");
+                    var password = "Zoha!bButt";
+                    var sub = subject;
+                    var body = message;
+                    var smtp = new SmtpClient
+                    {
+                        Host = "smtp.gmail.com",
+                        Port = 587,
+                        EnableSsl = true,
+                        DeliveryMethod = SmtpDeliveryMethod.Network,
+                        UseDefaultCredentials = false,
+                        Credentials = new NetworkCredential(senderEmail.Address, password)
+                    };
+                    using (var mess = new MailMessage(senderEmail, receiverEmail)
+                    {
+                        Subject = subject,
+                        Body = body
+                    })
+                    {
+                        smtp.Send(mess);
+                    }
+                    return View();
+                }
+            }
+            catch (Exception)
+            {
+                ViewBag.Error = "Some Error";
+            }
+            return View();
+        }
+    }
+}
---
+++ UPDowload/Controllers/RegisterController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using UPDowload.ModelData;
+
+namespace UPDowload.Controllers
+{
+    public class RegisterController : Controller
+    {
+
+        updownloadEntitiesModel db = new updownloadEntitiesModel();
+        // GET: Register
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public JsonResult SaveData(SiteUser model)
+        {
+            db.SiteUsers.Add(model);
+            db.SaveChanges();
+            return Json("Registeration Successfull", JsonRequestBehavior.AllowGet);
+
+        }
+    }
+}
