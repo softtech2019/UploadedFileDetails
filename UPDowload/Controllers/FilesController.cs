aec9128a939d06d96adf24b9185df9b5c9415c0b
rudph1 <rudph1@ac.uk>
2019-09-03 07:15:27 +0100

    file-up-downl tech

---
+++ UPDowload/Controllers/FilesController.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.IO;
+using System.Web;
+using System.Web.Mvc;
+using UPDowload.Models;
+
+namespace UPDowload.Controllers
+{
+    public class FilesController : Controller
+    {
+        string conString = "Data Source=(LocalDB)\\MSSQLLocalDB;Initial Catalog =updownload; integrated security=true;";
+        // GET: Files
+        public ActionResult Index(FileUpload model)
+        {
+            List<FileUpload> list = new List<FileUpload>();
+            DataTable dtFiles = GetFileDetails();
+            foreach (DataRow dr in dtFiles.Rows)
+            {
+                list.Add(new FileUpload
+                {
+                    FileId = @dr["SQLID"].ToString(),
+                    FileName = @dr["FILENAME"].ToString(),
+                    FileUrl = @dr["FILEURL"].ToString()
+                });
+            }
+            model.FileList = list;
+            return View(model);
+        }
+
+        [HttpPost]
+        public ActionResult Index(HttpPostedFileBase files)
+        {
+            FileUpload model = new FileUpload();
+            List<FileUpload> list = new List<FileUpload>();
+            DataTable dtFiles = GetFileDetails();
+            foreach (DataRow dr in dtFiles.Rows)
+            {
+                list.Add(new FileUpload
+                {
+                    FileId = @dr["SQLID"].ToString(),
+                    FileName = @dr["FILENAME"].ToString(),
+                    FileUrl = @dr["FILEURL"].ToString()
+                });
+            }
+            model.FileList = list;
+
+            if (files != null)
+            {
+                var Extension = Path.GetExtension(files.FileName);
+                var fileName = "my-file-" + DateTime.Now.ToString("yyyyMMddHHmmssfff") + Extension;
+                string path = Path.Combine(Server.MapPath("~/UploadedFiles"), fileName);
+                model.FileUrl = Url.Content(Path.Combine("~/UploadedFiles/", fileName));
+                model.FileName = fileName;
+
+                if (SaveFile(model))
+                {
+                    files.SaveAs(path);
+                    TempData["AlertMessage"] = "Uploaded Successfully !!";
+                    return RedirectToAction("Index", "Files");
+                }
+                else
+                {
+                    ModelState.AddModelError("", "Error In Add File. Please Try Again !!!");
+                }
+            }
+            else
+            {
+                ModelState.AddModelError("", "Please Choose Correct File Type !!");
+                return View(model);
+            }
+            return RedirectToAction("Index", "Files");
+        }
+
+        private DataTable GetFileDetails()
+        {
+            DataTable dtData = new DataTable();
+            SqlConnection con = new SqlConnection(conString);
+            con.Open();
+            SqlCommand command = new SqlCommand("Select * From tblFileDetails", con);
+            SqlDataAdapter da = new SqlDataAdapter(command);
+            da.Fill(dtData);
+            con.Close();
+            return dtData;
+        }
+
+        private bool SaveFile(FileUpload model)
+        {
+            string strQry = "INSERT INTO tblFileDetails (FileName,FileUrl) VALUES('" +
+                model.FileName + "','" + model.FileUrl + "')";
+            SqlConnection con = new SqlConnection(conString);
+            con.Open();
+            SqlCommand command = new SqlCommand(strQry, con);
+            int numResult = command.ExecuteNonQuery();
+            con.Close();
+            if (numResult > 0)
+                return true;
+            else
+                return false;
+        }
+
+
+        public ActionResult DownloadFile(string filePath)
+        {
+            string fullName = Server.MapPath("~" + filePath);
+
+            byte[] fileBytes = GetFile(fullName);
+            return File(
+                fileBytes, System.Net.Mime.MediaTypeNames.Application.Octet, filePath);
+        }
+
+        byte[] GetFile(string s)
+        {
+            FileStream fs = System.IO.File.OpenRead(s);
+            byte[] data = new byte[fs.Length];
+            int br = fs.Read(data, 0, data.Length);
+            if (br != fs.Length)
+                throw new IOException(s);
+            return data;
+        }
+    }
+
+}
